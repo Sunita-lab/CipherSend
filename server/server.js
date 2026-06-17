67872af63e8fed8c69cf18090ec0e3dc78785472
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-06-18 00:01:16 +0530

    fix: prepare for deployment

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected!');
     startCleanupJob();
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 5000, () => {
       console.log(`Server running on port ${process.env.PORT}`);
     });
   })
